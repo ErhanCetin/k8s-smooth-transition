57e2ad72c185b0da316a33c769359a18d9075cde
Erhan Cetin <e.cetin@netconomy.net>
2020-07-24 22:51:54 +0200

    MK8S-0001 create python project to fetch news from newsapi and write them into activemq

---
+++ news-tracker-job/org/news/tracker/NewsApi.py
@@ -0,0 +1,43 @@
+import datetime
+import json
+import os
+from time import sleep
+
+import requests
+import stomp
+
+# reference : https://newsapi.org/docs/client-libraries/python
+
+fromDate = datetime.date.today() - datetime.timedelta(2)
+toDate = datetime.date.today()
+# connection for newsapi.org
+news_url_for_everthing = 'https://newsapi.org/v2/everything'
+
+# parameter reference https://newsapi.org/docs/endpoints/everything
+payload_for_everything = {'q': 'technology', 'language': 'en', 'sortBy': 'publishedAt', 'from': fromDate, 'to': toDate}
+
+# check if null then use sample authorization
+authorization = os.environ.get('NEWSAPI-AUTHORIZATION')
+if authorization is None:
+    authorization = 'db87162d00af4d1bb4c8031ad1cf22f5'
+
+headers = {'Authorization': authorization}
+
+response = requests.get(url=news_url_for_everthing, headers=headers, params=payload_for_everything)
+pretty_json_output = json.dumps(response.json(), indent=4)
+print(pretty_json_output)
+
+response_json_string = json.dumps(response.json())
+response_dict = json.loads(response_json_string)
+print(response_dict)
+
+sleep(5)
+
+## put news data to activemq
+activemqHostName = os.environ.get('ACTIVEMQ-HOST')
+activemqPort = os.environ.get('ACTIVEMQ-PORT')
+
+conn = stomp.Connection([(activemqHostName, activemqPort)])
+conn.connect('admin', 'admin', wait=False)
+conn.send(body=response_json_string, destination='/queue/news-que')
+conn.disconnect()
