fc5e14bbb817b7a098f2415d856780d3f3395a98
Erhan Cetin <e.cetin@netconomy.net>
2020-11-13 18:11:02 +0100

    K8S-0015 changes fe

--- news-tracker-fe/pages/api/news.js
+++ news-tracker-fe/pages/api/news.js
@@ -4,8 +4,7 @@ import getConfig from 'next/config'
 const { serverRuntimeConfig, publicRuntimeConfig } = getConfig()
 
 export default (req, res) => {
-  //return fetch('http://'+serverRuntimeConfig.NEWS_API_HOST_NAME+':'+serverRuntimeConfig.NEWS_API_HOST_PORT+'/news/getAll')
-  return fetch('http://127.0.0.1:61560/news/getAll')
+  return fetch('http://'+serverRuntimeConfig.NEWS_API_HOST_NAME+':'+serverRuntimeConfig.NEWS_API_HOST_PORT+'/news/getAll')
   .then((response) => response.json())
   .then((data) => {
     res.statusCode = 200;
